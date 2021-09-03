7f6a1174e46506239449ddd6a325666f4e277556
Toleek <tom.0.0.7@yandex.ru>
2021-09-03 14:44:38 +0300

    Task guessing game completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		let min = null;
+		let max = null;
+		let result = null;
+	}
 
     setRange(min, max) {
-
+		this.min = min;
+		this.max = max;
     }
 
     guess() {
-
+		this.result = Math.round((this.min + this.max) / 2);
+		return this.result;
     }
 
     lower() {
-
+		this.max = this.result;
     }
 
     greater() {
-
+		this.min = this.result;
     }
 }
 
